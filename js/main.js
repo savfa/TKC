b50dfb8fa7996d32ab3f17bb9d5100efc74c5de7
Evgeniy <savfa@yandex.ru>
2019-09-28 00:16:05 +0300

    Animated section clients

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 $(function () {
+
+    //clients toggle
+    let clientsAgentBlock = $('.clients-agent__block');
+    let clientsClientBlock = $('.clients-client__block');
+    let train = $('.clients');
+    clientsClientBlock.hide();
+    $('.switching__link span').on('click', function () {
+        if($(this).css('left') == '35px') {
+            $(this).animate({'left':'3px'},500);
+            clientsClientBlock.hide();
+            clientsAgentBlock.fadeIn().css({'display':'flex', 'height':'470px'});
+            train.animate({'backgroundPositionX':'-680px'},1500);
+        } else {
+            $(this).animate({'left':'35px'},500);
+            clientsAgentBlock.hide();
+            clientsClientBlock.fadeIn().css({'display':'flex', 'height':'470px'});
+            train.animate({'backgroundPositionX':'1500px'},1500);
+        }
+    });
+
     //sale block show
     $('.sales__switching li').on('click', function () {
         $('.sales__block').hide();
