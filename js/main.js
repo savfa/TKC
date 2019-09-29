1b3684f0b824a01bab9214da2e3bc0bc9a0c1b32
garmoni <opasyura@gmail.com>
2019-09-29 15:25:40 +0300

    add block about us

--- js/main.js
+++ js/main.js
@@ -3,28 +3,28 @@ $(function () {
     //clients toggle
     let clientsAgentBlock = $('.clients-agent__block');
     let clientsClientBlock = $('.clients-client__block');
-    let buisnessClientsBlock = $('.buisness_clients_wrap');
-    let buisnessAgentBlock = $('.buisness_agent_wrap');
+    let businessClientsBlock = $('.business_clients_wrap');
+    let businessAgentBlock = $('.business_agent_wrap');
     let train = $('.clients');
     clientsClientBlock.hide();
-    buisnessClientsBlock.hide();
+    businessClientsBlock.hide();
     $('.switching__link span').on('click', function () {
         if($(this).css('left') == '35px') {   // show Agents
             $(this).animate({'left':'3px'},500);
             clientsClientBlock.hide();
             clientsAgentBlock.fadeIn().css({'display':'flex', 'height':'470px'});
             train.animate({'backgroundPositionX':'-680px'},1500);
-            buisnessClientsBlock.hide();
-            buisnessAgentBlock.show().find('.buisness_img').attr('src','img/ticket_sale.png').parents()
-                .find('.buisness_clients-item:first-child div').addClass('active');
+            businessClientsBlock.hide();
+            businessAgentBlock.show().find('.business_img').attr('src','img/ticket_sale.png').parents()
+                .find('.business_clients-item:first-child div').addClass('active');
         } else {   // show Clients
             $(this).animate({'left':'35px'},500);
             clientsAgentBlock.hide();
             clientsClientBlock.fadeIn().css({'display':'flex', 'height':'470px'});
             train.animate({'backgroundPositionX':'1500px'},1500);
-            buisnessAgentBlock.hide();
-            buisnessClientsBlock.show().find('.buisness_img').attr('src','img/agriculture.png').parents()
-                .find('.buisness_clients-list:first-child .buisness_clients-item:first-child div').addClass('active');
+            businessAgentBlock.hide();
+            businessClientsBlock.show().find('.business_img').attr('src','img/agriculture.png').parents()
+                .find('.business_clients-list:first-child .business_clients-item:first-child div').addClass('active');
         }
     });
 
@@ -37,12 +37,12 @@ $(function () {
         $(this).children('div').addClass('circleRed');
         $('.sales__switching li').not(this).removeClass('active').children('div').removeClass('circleRed');
     });
-    //buisness img toggle
+    //business img toggle
 
-    $('.buisness_clients-item').on('click', function () {
+    $('.business_clients-item').on('click', function () {
         let toImg = $(this).attr('data-show_img');
-        $('.buisness_clients-item div').not(this).removeClass('active');
+        $('.business_clients-item div').not(this).removeClass('active');
         $(this).find('div').addClass('active');
-        $('.buisness_img').attr('src','img/'+toImg+'.png');
+        $('.business_img').attr('src','img/'+toImg+'.png');
     });
 });
