a41299853296081799ca514c50352849246a0db1
Evgeniy <savfa@yandex.ru>
2019-09-29 22:40:26 +0300

    Add buisness slider (I am progger)

--- js/main.js
+++ js/main.js
@@ -3,28 +3,19 @@ $(function () {
     //clients toggle
     let clientsAgentBlock = $('.clients-agent__block');
     let clientsClientBlock = $('.clients-client__block');
-    let businessClientsBlock = $('.business_clients_wrap');
-    let businessAgentBlock = $('.business_agent_wrap');
-    let train = $('.clients');
     clientsClientBlock.hide();
-    businessClientsBlock.hide();
+    let train = $('.clients');
     $('.switching__link span').on('click', function () {
         if($(this).css('left') == '35px') {   // show Agents
             $(this).animate({'left':'3px'},500);
             clientsClientBlock.hide();
             clientsAgentBlock.fadeIn().css({'display':'flex', 'height':'470px'});
             train.animate({'backgroundPositionX':'-680px'},1500);
-            businessClientsBlock.hide();
-            businessAgentBlock.show().find('.business_img').attr('src','img/ticket_sale.png').parents()
-                .find('.business_clients-item:first-child div').addClass('active');
         } else {   // show Clients
             $(this).animate({'left':'35px'},500);
             clientsAgentBlock.hide();
             clientsClientBlock.fadeIn().css({'display':'flex', 'height':'470px'});
             train.animate({'backgroundPositionX':'1500px'},1500);
-            businessAgentBlock.hide();
-            businessClientsBlock.show().find('.business_img').attr('src','img/agriculture.png').parents()
-                .find('.business_clients-list:first-child .business_clients-item:first-child div').addClass('active');
         }
     });
 
@@ -37,12 +28,82 @@ $(function () {
         $(this).children('div').addClass('circleRed');
         $('.sales__switching li').not(this).removeClass('active').children('div').removeClass('circleRed');
     });
-    //business img toggle
 
+    //business clients toggle and slide img
+    let businessClientsBlock = $('.business_clients_wrap');
+    let businessAgentBlock = $('.business_agent_wrap');
+    businessClientsBlock.hide();
+    $('.arrow_prev').on('click', hendlePrev);
+    $('.arrow_next').on('click', hendleNext);
+    $('.arrow_next').hover(()=>{
+            if($('.business_clients_wrap').is(":visible")) {
+                $('.arrow_next').removeClass('scale')
+            }else {$('.arrow_next').addClass('scale')}
+        },
+        ()=>{$('.arrow_next').removeClass('scale')}
+    );
+    $('.arrow_prev').hover(()=>{
+            if($('.business_agent_wrap').is(":visible")) {
+                $('.arrow_prev').removeClass('scale')
+            }else {$('.arrow_prev').addClass('scale')}
+        },
+        ()=>{$('.arrow_prev').removeClass('scale')}
+    );
+    function hendlePrev(e) {
+        businessClientsBlock.hide();
+        businessAgentBlock.show().find('.business_img').attr('src', 'img/ticket_sale.png').parents()
+            .find('.business_clients-list:first-child .business_clients-item:first-child div').addClass('active');
+        if(businessAgentBlock.is(":visible")) {
+            $(this).unbind(e,hendlePrev);
+            $('.arrow_next').bind('click',hendleNext);
+            imgNameArray=[];
+            currentSlide = 0;
+            $('.business_agent_wrap .business_clients-item').map((i,item)=>{
+                imgNameArray.push($(item).attr('data-show_img'));
+                return true
+            });
+        }
+    }
+
+    function hendleNext(e) {
+        businessAgentBlock.hide();
+        businessClientsBlock.show().find('.business_img').attr('src','img/agriculture.png').parents()
+            .find('.business_clients-list:first-child .business_clients-item:first-child div').addClass('active');
+        if(businessClientsBlock.is(":visible")) {
+            $(this).unbind(e,hendleNext);
+            $('.arrow_prev').bind('click',hendlePrev);
+            imgNameArray=[];
+            currentSlide = 0;
+            $('.business_clients_wrap .business_clients-item').map((i,item)=>{
+                imgNameArray.push($(item).attr('data-show_img'));
+                return true
+            });
+        }
+    }
+
+    //buisness slider
+    var imgNameArray=[];
+    var currentSlide = 0;
+    $('.business_agent_wrap .business_clients-item').map((i,item)=>{
+        imgNameArray.push($(item).attr('data-show_img'));
+        return true
+    });
+    setInterval(()=> {
+        imgNameArray[currentSlide].className = 'slide';
+            $('.business_img').attr('src','img/'+imgNameArray[currentSlide]+'.png');
+            if ( $(`[data-show_img=${imgNameArray[currentSlide]}`) ) {
+                let el = $(`[data-show_img=${imgNameArray[currentSlide]}`);
+                $('.business_clients-item div').not(el).removeClass('active');
+                $(el).children().addClass('active');
+            }
+            currentSlide++;
+            if(currentSlide == imgNameArray.length) {currentSlide=0}
+    },2000);
     $('.business_clients-item').on('click', function () {
         let toImg = $(this).attr('data-show_img');
         $('.business_clients-item div').not(this).removeClass('active');
         $(this).find('div').addClass('active');
         $('.business_img').attr('src','img/'+toImg+'.png');
+        currentSlide = imgNameArray.indexOf( toImg);
     });
 });
